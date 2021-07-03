923c2703bc451588bf89c027fb7f261f76704fc5
Nirvan-sharma <78906217+Nirvan-sharma@users.noreply.github.com>
2021-07-03 16:04:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+prediction="";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+var camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version',ml5.version);
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/hb_5pnEy8/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("model loaded");
+}
+
+function speak(){
+var synth=window.speechSynthesis;
+speak_data_1="the prediction is "+prediction;
+var utterThis=new SpeechSynthesisUtterance(speak_data_1);
+synth.speak(utterThis);
+}
